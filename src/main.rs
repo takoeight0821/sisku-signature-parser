5ecb05ba4cff8abdd8073de9a5f2a23445b80b73
Yuya Kono <takohati0821@gmail.com>
2022-07-01 14:33:07 +0900

    Add some experimental code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use tree_sitter::{Language, Parser};
+
+extern "C" {
+    fn tree_sitter_javascript() -> Language;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut parser = Parser::new();
+    let language = unsafe { tree_sitter_javascript() };
+    parser.set_language(language).unwrap();
+
+    let source_code = "function add(x, y) { return x + y; }";
+    let tree = parser.parse(source_code, None).unwrap();
+
+    println!("{}", source_code);
+    println!("{}", tree.root_node().to_sexp());
+
+    let source_code = "add(x, y)";
+    let tree = parser.parse(source_code, None).unwrap();
+
+    println!("{}", source_code);
+    println!("{}", tree.root_node().to_sexp());
+
+    let source_code = "function add(x, y)";
+    let tree = parser.parse(source_code, None).unwrap();
+
+    println!("{}", source_code);
+    println!("{}", tree.root_node().to_sexp());
 }
